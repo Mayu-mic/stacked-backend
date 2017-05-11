5d4ffbebbbac1cbc4d24fae31e98391f24ee08eb
Ryo Koizumi <koizumiryo@gmail.com>
2017-05-11 19:18:59 +0900

    add itemstar

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+  has_many :stars, class_name: 'ItemStar'
   belongs_to :list
   belongs_to :created_by, class_name: 'User'
 end
---
+++ app/models/item_star.rb
@@ -0,0 +1,4 @@
+class ItemStar < ApplicationRecord
+  belongs_to :item
+  belongs_to :created_by, class_name: 'User'
+end
