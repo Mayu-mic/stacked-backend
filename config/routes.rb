11a4d7646e08bcb6cc6e6ae073bd265c0f9201f5
Ryo Koizumi <koizumiryo@gmail.com>
2017-05-16 00:24:51 +0900

    サインアウトできるように

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
     end
   end
 
-  mount_devise_token_auth_for 'User', at: 'auth', skip: [:sessions]
+  mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
