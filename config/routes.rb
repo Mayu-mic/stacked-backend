d9e930dbadad46f4fcc28418b5b90f21aa6bc601
Ryo Koizumi <ryo.koizumi@soinn.com>
2017-05-15 11:05:43 +0900

    delete star機能を殺す

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   resources :lists, shallow: true, only: [:index, :create, :update] do
     resources :items, only: [:index, :show, :create, :update, :destroy] do
       post '/star', to: 'items#addstar'
-      delete '/star', to: 'items#delstar'
+      # delete '/star', to: 'items#delstar'
       resources :comments, only: [:index, :create, :destroy] do
         post '/star', to: 'comments#addstar'
-        delete '/star', to: 'comments#delstar'
+        # delete '/star', to: 'comments#delstar'
       end
     end
   end
